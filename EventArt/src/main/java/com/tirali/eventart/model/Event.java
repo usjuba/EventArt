56d01f1382b95181a9fc7d8941447c17e245a50a
Hristo Savov <bursar@abv.bg>
2013-06-15 20:05:54 +0300

    initial code
    initial code

---
+++ EventArt/src/main/java/com/tirali/eventart/model/Event.java
@@ -0,0 +1,172 @@
+package com.tirali.eventart.model;
+
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.OneToOne;
+import com.tirali.eventart.model.User;
+import javax.persistence.CascadeType;@Entity public class Event implements java.io.Serializable {
+
+	@Id
+	private @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	Long id = null;
+	@Version
+	private @Column(name = "version")
+	int version = 0;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		if (this == that) {
+			return true;
+		}
+		if (that == null) {
+			return false;
+		}
+		if (getClass() != that.getClass()) {
+			return false;
+		}
+		if (id != null) {
+			return id.equals(((Event) that).id);
+		}
+		return super.equals(that);
+	}
+
+	@Override
+	public int hashCode() {
+		if (id != null) {
+			return id.hashCode();
+		}
+		return super.hashCode();
+	}
+
+	@Column
+	private String name;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@Column
+	private String type;
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(final String type) {
+		this.type = type;
+	}
+
+	@Column
+	private String description;
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	@Column
+	private String theme;
+
+	public String getTheme() {
+		return this.theme;
+	}
+
+	public void setTheme(final String theme) {
+		this.theme = theme;
+	}
+
+	private @Temporal(TemporalType.DATE)
+	Date startDate;
+
+	public Date getStartDate() {
+		return this.startDate;
+	}
+
+	public void setStartDate(final Date startDate) {
+		this.startDate = startDate;
+	}
+
+	private @Temporal(TemporalType.DATE)
+	Date endDate;
+
+	public Date getEndDate() {
+		return this.endDate;
+	}
+
+	public void setEndDate(final Date endDate) {
+		this.endDate = endDate;
+	}
+
+	@Column
+	private String place;
+
+	public String getPlace() {
+		return this.place;
+	}
+
+	public void setPlace(final String place) {
+		this.place = place;
+	}
+
+	private @OneToOne(cascade=CascadeType.ALL) User owner;
+
+	public User getOwner() {
+		return this.owner;
+	}
+
+	public void setOwner(final User owner) {
+		this.owner = owner;
+	}
+
+	public String toString() {
+		String result = "";
+		if (name != null && !name.trim().isEmpty())
+			result += name;
+		if (type != null && !type.trim().isEmpty())
+			result += " " + type;
+		if (description != null && !description.trim().isEmpty())
+			result += " " + description;
+		if (theme != null && !theme.trim().isEmpty())
+			result += " " + theme;
+		if (place != null && !place.trim().isEmpty())
+			result += " " + place;
+		return result;
+	}
+
+	public void newUser() {
+		this.owner = new User();
+	} }
---
+++ EventArt/src/main/java/com/tirali/eventart/model/User.java
@@ -0,0 +1,104 @@
+package com.tirali.eventart.model;
+
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;@Entity public class User implements java.io.Serializable {
+
+	@Id
+	private @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	Long id = null;
+	@Version
+	private @Column(name = "version")
+	int version = 0;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		if (this == that) {
+			return true;
+		}
+		if (that == null) {
+			return false;
+		}
+		if (getClass() != that.getClass()) {
+			return false;
+		}
+		if (id != null) {
+			return id.equals(((User) that).id);
+		}
+		return super.equals(that);
+	}
+
+	@Override
+	public int hashCode() {
+		if (id != null) {
+			return id.hashCode();
+		}
+		return super.hashCode();
+	}
+
+	@Column
+	private String username;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	@Column
+	private String password;
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	@Column
+	private String email;
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		String result = "";
+		if (username != null && !username.trim().isEmpty())
+			result += username;
+		if (password != null && !password.trim().isEmpty())
+			result += " " + password;
+		if (email != null && !email.trim().isEmpty())
+			result += " " + email;
+		return result;
+	} }
